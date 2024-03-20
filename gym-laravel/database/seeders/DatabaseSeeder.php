57bffa267bac89473d07b2a2fe1d55116ba48160
Giulius94 <giuliuscb@protonmail.com>
2024-03-20 17:03:28 +0100

    ok

--- gym-laravel/database/seeders/DatabaseSeeder.php
+++ gym-laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,15 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+         
+
+        $this->call([
+            AttivitaSeeder::class,
+            OrariSeeder::class,
+            PrenotazioniSeeder::class
+        ]);
+
+        User::factory(6)->create(); 
+        
     }
 }
